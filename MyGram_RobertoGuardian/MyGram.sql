48e2bfac8cc899403403f2b41ab8bc77cebe6417
Roberto Guardian <>
2021-05-05 15:42:30 -0600

    My 4th Assignment of sql

---
+++ MyGram_RobertoGuardian/MyGram.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE MyGram;
+
+USE MyGram;
+
+CREATE TABLE Image(
+   ImageId          int,           
+   ImageName         varchar(500),  
+   ImagePath         varchar(500),
+   ImageDescription  varchar(500),     
+   ImageAltText      varchar(500)
+   );
